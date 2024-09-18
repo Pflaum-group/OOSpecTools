ab047de1a44d96d17d49dbbebf445dbfa99de140
HammerSeb <sebastian@hammer-mail.com>
2024-09-18 14:45:07 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='OOSpecTools',
-      version='0.0.2',
+      version='0.1.0',
       description='Tools for OceanOptics spectrometers',
       long_description=open('README.md').read(),
       long_description_content_type ='text/markdown',
