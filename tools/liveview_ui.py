2402babfd1b52b5438410127e7de4704969f95f5
HammerSeb <sebastian@hammer-mail.com>
2024-09-18 14:44:23 +0200

    hotkeys added

--- tools/liveview_ui.py
+++ tools/liveview_ui.py
@@ -13,9 +13,11 @@ class LiveView(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
 
         self._aqtime = aqtime
+        self._AQTIME_INCREMENT = 10000
         self._wavelength = wavelength
         self._savepath = savepath
         self._savecounter = 0
+        self._dcflag = False
         
         ### Build UI
         self.window = pg.GraphicsLayoutWidget(show=True)
@@ -24,8 +26,8 @@ class LiveView(QtWidgets.QMainWindow):
 
         # Define plot Layout
         self.p1 = self.window.addPlot()
-        self.p1.setLabel('left', 'intensity', units='cts')
-        self.p1.setLabel('bottom', 'wavelength', units='nm')
+        self.p1.setLabel('left', 'intensity [a.u.]')
+        self.p1.setLabel('bottom', 'wavelength [nm]')
         self.p1.setXRange(self.wavelength[0],self.wavelength[1])
         self.p1.enableAutoRange('x', False)
         self.curve = self.p1.plot(pen="r")
@@ -37,7 +39,15 @@ class LiveView(QtWidgets.QMainWindow):
         self.shortcut_add_save_spectrum = QtWidgets.QShortcut(QKeySequence("s"), self)
         self.shortcut_add_save_spectrum.activated.connect(self.save_spectrum)
 
+        self.shortcut_add_dark_correction = QtWidgets.QShortcut(QKeySequence("d"), self)
+        self.shortcut_add_dark_correction.activated.connect(self.darkcorrection)
         
+        self.shortcut_add_increase_aqtime = QtWidgets.QShortcut(QKeySequence("+"), self)
+        self.shortcut_add_increase_aqtime.activated.connect(self.increase_aqtime)
+
+        self.shortcut_add_decrease_aqtime = QtWidgets.QShortcut(QKeySequence("-"), self)
+        self.shortcut_add_decrease_aqtime.activated.connect(self.decrease_aqtime)
+
         ### Connect and start spectrometer
         self.spectrometer = TempSpec(self.aqtime)
 
@@ -51,12 +61,37 @@ class LiveView(QtWidgets.QMainWindow):
     def update(self):
         _int = self.spectrometer.aquire_spectrum()
         self.curve.setData(y=_int, x=self.spectrometer.wavelength)
+        self.p1.setYRange(0.95*_int[:2068].min(),1.05*_int[:2068].max())
 
     @pyqtSlot()
     def save_spectrum(self):
         np.save(path.join(self.savepath,f"spectrum_{self._savecounter:03d}.npy"), 
                 np.vstack((self.spectrometer.wavelength, self.spectrometer.aquire_spectrum())))
         self._savecounter += 1
+    
+    def increase_aqtime(self):
+        self.aqtime = self.aqtime + self._AQTIME_INCREMENT
+        self.updateTimer.stop()
+        self.spectrometer.set_aqtime(self.aqtime)
+        self.updateTimer.start(int(self.aqtime/1000))
+    
+    def decrease_aqtime(self):
+        newaqtime = self.aqtime - self._AQTIME_INCREMENT
+        if newaqtime >= 10000:
+            self.aqtime = newaqtime
+            self.updateTimer.stop()
+            self.spectrometer.set_aqtime(self.aqtime)
+            self.updateTimer.start(int(self.aqtime/1000))
+    
+    def darkcorrection(self):
+        if not self._dcflag:
+            self._dcflag = True
+            self.spectrometer.dcflag = self._dcflag
+        else:
+            self._dcflag = False
+            self.spectrometer.dcflag = self._dcflag
+        
+
 
     ### Attributes
     @property
--- tools/spectools.py
+++ tools/spectools.py
@@ -10,17 +10,28 @@ class TempSpec():
         self.spec = Spectrometer.from_first_available()
         self.spec.integration_time_micros(aq_time)
         self._wavelength = self.spec.wavelengths()
+        self._dcflag = False
 
     def aquire_spectrum(self) -> np.ndarray:
-        return self.spec.intensities()
+        return self.spec.intensities(correct_dark_counts = self.dcflag)
     
     def set_aqtime(self, aqtime: int):
         self.spec.integration_time_micros(aqtime)
+        print(f"aq_time set to {aqtime}")
 
     def close(self):
         self.spec.close()
+
     ### Properties
     
     @property
     def wavelength(self) -> np.ndarray:
         return self._wavelength
+    
+    @property
+    def dcflag(self) -> bool:
+        return self._dcflag
+    
+    @dcflag.setter
+    def dcflag(self, flag: bool) -> None:
+        self._dcflag = flag
